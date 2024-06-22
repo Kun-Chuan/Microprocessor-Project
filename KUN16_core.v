667b2bf8cdce7399fd3b031f959a6fa6e994c1e8
Kun-Chuan <hungkunchuan@ufl.edu>
2024-06-22 15:18:10 +0800

    Update KUN16_core.v

--- KUN16_core.v
+++ KUN16_core.v
@@ -1,21 +1,22 @@
 module KUN16_core (clk, 
-						 rst, 
-						 pc_out, 
-						 alu_s, 
-						 cur_inst_type,
-						 rf_wdata,
-						 rf_rdata0,
-						 rf_rdata1,
-						 rf_wen,
-						 dm_wen,
-						 dm_rdata,
-						 dm_wdata,
-						 csr_wen, 
-						 csr_rdata,
-						 csr_wdata,
-						 inst_illegal,
-						 halt
-						 );
+		   rst, 
+		   pc_out, 
+		   alu_s, 
+		   cur_inst_type,
+		   rf_wdata,
+		   rf_rdata0,
+		   rf_rdata1,
+		   rf_wen,
+		   dm_wen,
+		   dm_rdata,
+		   dm_wdata,
+		   csr_wen, 
+		   csr_rdata,
+		   csr_wdata,
+		   inst_illegal,
+		   halt
+		   );
+	
 input clk, rst;
 output [15:0]pc_out, alu_s;
 output [4:0]cur_inst_type;
@@ -52,64 +53,64 @@ assign dm_wdata = rf_rdata1;
 
 
 PC PC0 (.clk(clk), 
-		  .rst(rst), 
-		  .inst_illegal(inst_illegal),
-		  .halt(halt),
-		  .pc_in(pc_in), 
-		  .pc_out(pc_out));
+	.rst(rst), 
+	.inst_illegal(inst_illegal),
+	.halt(halt),
+	.pc_in(pc_in), 
+	.pc_out(pc_out));
 		  
 IM IM0(.clk(clk), 
-		 .rst(rst), 
-		 .pc(pc_out), 
-		 .inst(inst));
+       .rst(rst), 
+       .pc(pc_out), 
+       .inst(inst));
 		 
-ID_controller ID0(   .inst(inst),
-							.branch_taken(branch_taken),
-							.write_pc2reg(write_pc2reg),
-							.csr_wen(csr_wen),
-							.csr_rdata(csr_rdata),
-							.dm_wen(dm_wen),
-							.wrtie_mem_result2reg(wrtie_mem_result2reg),
-							.alu_b_src_sel_0(alu_b_src_sel_0),
-							.alu_a_src_sel_0(alu_a_src_sel_0),
-							.alu_op(alu_op),
-							.rf_wen(rf_wen),
-							.rf_raddr0(rf_raddr0),
-							.rf_raddr1(rf_raddr1),
-							.rf_waddr(rf_waddr),
-							.alu_imm(alu_imm),
-							.cur_inst_type(cur_inst_type),// use for debug only
-							.inst_illegal(inst_illegal),
-							.halt(halt)
-							);
+ID_controller ID0(.inst(inst),
+		  .branch_taken(branch_taken),
+		  .write_pc2reg(write_pc2reg),
+		  .csr_wen(csr_wen),
+		  .csr_rdata(csr_rdata),
+		  .dm_wen(dm_wen),
+		  .wrtie_mem_result2reg(wrtie_mem_result2reg),
+		  .alu_b_src_sel_0(alu_b_src_sel_0),
+		  .alu_a_src_sel_0(alu_a_src_sel_0),
+		  .alu_op(alu_op),
+		  .rf_wen(rf_wen),
+		  .rf_raddr0(rf_raddr0),
+		  .rf_raddr1(rf_raddr1),
+		  .rf_waddr(rf_waddr),
+		  .alu_imm(alu_imm),
+		  .cur_inst_type(cur_inst_type),// use for debug only
+		  .inst_illegal(inst_illegal),
+		  .halt(halt)
+		  );
 							
 RF RF0(.clk(clk), 
-		 .rst(rst), 
-		 .raddr0(rf_raddr0), 
-		 .raddr1(rf_raddr1), 
-		 .waddr(rf_waddr), 
-		 .wen(rf_wen), 
-		 .wdata(rf_wdata), 
-		 .rdata0(rf_rdata0), 
-		 .rdata1(rf_rdata1));
+       .rst(rst), 
+       .raddr0(rf_raddr0), 
+       .raddr1(rf_raddr1), 
+       .waddr(rf_waddr), 
+       .wen(rf_wen), 
+       .wdata(rf_wdata), 
+       .rdata0(rf_rdata0), 
+       .rdata1(rf_rdata1));
 							
 ALU ALU0(.A(alu_a), 
-			.B(alu_b), 
-			.op(alu_op), 
-			.S(alu_s), 
-			.coms(csr_wdata));
+	 .B(alu_b), 
+	 .op(alu_op), 
+	 .S(alu_s), 
+	 .coms(csr_wdata));
 			
 DM DM0(.clk(clk), 
-		 .rst(rst), 
-		 .addr(alu_s), 
-		 .wen(dm_wen), 
-		 .rdata(dm_rdata), 
-		 .wdata(dm_wdata));
+       .rst(rst), 
+       .addr(alu_s), 
+       .wen(dm_wen), 
+       .rdata(dm_rdata), 
+       .wdata(dm_wdata));
 
 CSR CSR0(.clk(clk), 
-			.rst(rst), 
-			.wen(csr_wen), 
-			.rdata(csr_rdata), 
-			.wdata(csr_wdata));		 
+	 .rst(rst), 
+	 .wen(csr_wen), 
+	 .rdata(csr_rdata), 
+	 .wdata(csr_wdata));		 
 							
 endmodule
